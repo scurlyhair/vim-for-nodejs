7f89e82a2d660cd1c3874d0bd431ebb276dcad5f
武云辉 <wuyunhuiace@gmail.com>
2016-09-05 03:44:52 +0800

    add:node test

---
+++ nodefortest/server.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+var server = http.createServer(function(request, response){
+	response.writeHead(200, {
+		"Content-Type": "text/plain"	
+	});
+	response.write("Hello World");
+	response.end();
+});
+server.listen(8888);
